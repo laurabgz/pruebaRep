da44c8f97a69b0a8803b62a31ed445a7e83be568
laura bravo <laurabravo@uma.es>
2025-06-29 23:02:37 +0200

    Added files pom.xml and src

---
+++ src/main/java/laurasoft/Statistics.java
@@ -0,0 +1,58 @@
+package laurasoft;
+
+import org.apache.commons.math3.stat.StatUtils;
+
+import java.util.List;
+
+/** Class including some statistics methods for arrays of numbers */
+public class Statistics {
+
+    public double maximumValue(double[] arrayOfNumbers) {
+        if (arrayOfNumbers.length == 0) {
+            throw new IllegalArgumentException("Array must not be empty");
+        }
+        double max = arrayOfNumbers[0];
+        for (int i = 1; i < arrayOfNumbers.length; i++) {
+            if (arrayOfNumbers[i] > max) {
+                max = arrayOfNumbers[i];
+            }
+        }
+        return max;
+    }
+
+    public double minimumValue(double[] arrayOfNumbers) {
+        if (arrayOfNumbers.length == 0) {
+            throw new IllegalArgumentException("Array must not be empty");
+        }
+        double min = arrayOfNumbers[0];
+        for (int i = 1; i < arrayOfNumbers.length; i++) {
+            if (arrayOfNumbers[i] < min) {
+                min = arrayOfNumbers[i];
+            }
+        }
+        return min;
+    }
+
+    /**
+     * Hola mundo
+     * @param arrayOfNumbers jelou
+     * @return bai
+     */
+    public double mean(double[] arrayOfNumbers) {
+        if (arrayOfNumbers.length == 0) {
+            throw new IllegalArgumentException("Array must not be empty");
+        }
+        double sum = 0.0;
+        for (double num : arrayOfNumbers) {
+            sum += num;
+        }
+        return sum / arrayOfNumbers.length;
+    }
+
+    public double median(double[] array0fNumbers) {
+        if (array0fNumbers.length == 0) {
+            throw new IllegalArgumentException("Array must not be empty");
+        }
+        return StatUtils.percentile(array0fNumbers, 50.0);
+    }
+}
